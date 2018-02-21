mozilla/cubeb-rs

Rework cubeb-backend crate.

cubeb-backend/src/capi.rs
@@ -3,10 +3,11 @@
 // This program is made available under an ISC-style license.  See the
 // accompanying file LICENSE for details
 
-use {Context, Stream};
-use cubeb_core::{DeviceId, DeviceType, StreamParams, ffi};
-use cubeb_core::binding::Binding;
+use {ContextOps, StreamOps};
+use cubeb_core::{ffi, DeviceCollectionRef, DeviceRef, DeviceType, StreamParams};
+use foreign_types::{ForeignType, ForeignTypeRef};
 use std::ffi::CStr;
+use std::mem;
 use std::os::raw::{c_char, c_int, c_void};
 
 // Helper macro for unwrapping `Result` values from rust-api calls
@@ -18,6 +19,16 @@ macro_rules! _try(
         Err(e) => return e.raw_code()
     })
 );
+
+macro_rules! as_opt_ref {
+    ($e:expr) => {
+        if $e.is_null() {
+            None
+        } else {
+            Some(ForeignTypeRef::from_ptr($e))
+        }
+    }
+}
 
 #[macro_export]
 macro_rules! capi_new(
@@ -47,111 +58,121 @@ macro_rules! capi_new(
             register_device_collection_changed: Some($crate::capi::capi_register_device_collection_changed::<$ctx>)
         }));
 
-pub unsafe extern "C" fn capi_init<CTX: Context>(
+pub unsafe extern fn capi_init<CTX: ContextOps>(
     c: *mut *mut ffi::cubeb,
     context_name: *const c_char,
-) -> c_int {
+) -> c_int
+{
     let anchor = &();
     let context_name = opt_cstr(anchor, context_name);
-    *c = _try!(CTX::init(context_name));
+    let context = _try!(CTX::init(context_name));
+    *c = context.as_ptr();
+    // Leaking pointer across C FFI
+    mem::forget(context);
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_get_backend_id<CTX: Context>(
+pub unsafe extern fn capi_get_backend_id<CTX: ContextOps>(
     c: *mut ffi::cubeb,
 ) -> *const c_char {
     let ctx = &mut *(c as *mut CTX);
     ctx.backend_id().as_ptr()
 }
 
-pub unsafe extern "C" fn capi_get_max_channel_count<CTX: Context>(
+pub unsafe extern fn capi_get_max_channel_count<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     max_channels: *mut u32,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
 
     *max_channels = _try!(ctx.max_channel_count());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_get_min_latency<CTX: Context>(
+pub unsafe extern fn capi_get_min_latency<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     param: ffi::cubeb_stream_params,
     latency_frames: *mut u32,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
-    let param = StreamParams::from_raw(&param);
-    *latency_frames = _try!(ctx.min_latency(&param));
+    let param = StreamParams::from(param);
+    *latency_frames = _try!(ctx.min_latency(param));
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_get_preferred_sample_rate<CTX: Context>(
+pub unsafe extern fn capi_get_preferred_sample_rate<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     rate: *mut u32,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
 
     *rate = _try!(ctx.preferred_sample_rate());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_get_preferred_channel_layout<CTX: Context>(
+pub unsafe extern fn capi_get_preferred_channel_layout<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     layout: *mut ffi::cubeb_channel_layout,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
 
     *layout = _try!(ctx.preferred_channel_layout()) as _;
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_enumerate_devices<CTX: Context>(
+pub unsafe extern fn capi_enumerate_devices<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     devtype: ffi::cubeb_device_type,
     collection: *mut ffi::cubeb_device_collection,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
     let devtype = DeviceType::from_bits_truncate(devtype);
-    *collection = _try!(ctx.enumerate_devices(devtype));
+    let collection = DeviceCollectionRef::from_ptr(collection);
+    _try!(ctx.enumerate_devices(devtype, collection));
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_device_collection_destroy<CTX: Context>(
+pub unsafe extern fn capi_device_collection_destroy<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     collection: *mut ffi::cubeb_device_collection,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
-
-    ctx.device_collection_destroy(collection);
+    let collection = DeviceCollectionRef::from_ptr(collection);
+    let _ = ctx.device_collection_destroy(collection);
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_destroy<CTX>(c: *mut ffi::cubeb) {
+pub unsafe extern fn capi_destroy<CTX>(c: *mut ffi::cubeb) {
     let _: Box<CTX> = Box::from_raw(c as *mut _);
 }
 
-pub unsafe extern "C" fn capi_stream_init<CTX: Context>(
+pub unsafe extern fn capi_stream_init<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     s: *mut *mut ffi::cubeb_stream,
     stream_name: *const c_char,
     input_device: ffi::cubeb_devid,
-    input_stream_params: *const ffi::cubeb_stream_params,
+    input_stream_params: *mut ffi::cubeb_stream_params,
     output_device: ffi::cubeb_devid,
-    output_stream_params: *const ffi::cubeb_stream_params,
+    output_stream_params: *mut ffi::cubeb_stream_params,
     latency_frames: u32,
     data_callback: ffi::cubeb_data_callback,
     state_callback: ffi::cubeb_state_callback,
     user_ptr: *mut c_void,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
     let anchor = &(); // for lifetime of stream_name as CStr
 
-    let input_device = DeviceId::from_raw(input_device);
-    let input_stream_params = input_stream_params.as_opt_ref();
-    let output_device = DeviceId::from_raw(output_device);
-    let output_stream_params = output_stream_params.as_opt_ref();
+    let input_stream_params = as_opt_ref!(input_stream_params);
+    let output_stream_params = as_opt_ref!(output_stream_params);
 
-    *s = _try!(ctx.stream_init(
+    let stream = _try!(ctx.stream_init(
         opt_cstr(anchor, stream_name),
         input_device,
         input_stream_params,
@@ -162,32 +183,31 @@ pub unsafe extern "C" fn capi_stream_init<CTX: Context>(
         state_callback,
         user_ptr
     ));
+    *s = stream.as_ptr();
+    // Leaking pointer across C FFI
+    mem::forget(stream);
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_destroy<STM>(s: *mut ffi::cubeb_stream) {
+pub unsafe extern fn capi_stream_destroy<STM>(s: *mut ffi::cubeb_stream) {
     let _ = Box::from_raw(s as *mut STM);
 }
 
-pub unsafe extern "C" fn capi_stream_start<STM: Stream>(
-    s: *mut ffi::cubeb_stream,
-) -> c_int {
+pub unsafe extern fn capi_stream_start<STM: StreamOps>(s: *mut ffi::cubeb_stream) -> c_int {
     let stm = &mut *(s as *mut STM);
 
     _try!(stm.start());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_stop<STM: Stream>(
-    s: *mut ffi::cubeb_stream,
-) -> c_int {
+pub unsafe extern fn capi_stream_stop<STM: StreamOps>(s: *mut ffi::cubeb_stream) -> c_int {
     let stm = &mut *(s as *mut STM);
 
     _try!(stm.stop());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_reset_default_device<STM: Stream>(
+pub unsafe extern fn capi_stream_reset_default_device<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
 ) -> c_int {
     let stm = &mut *(s as *mut STM);
@@ -196,71 +216,79 @@ pub unsafe extern "C" fn capi_stream_reset_default_device<STM: Stream>(
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_get_position<STM: Stream>(
+pub unsafe extern fn capi_stream_get_position<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
     position: *mut u64,
-) -> c_int {
+) -> c_int
+{
     let stm = &mut *(s as *mut STM);
 
     *position = _try!(stm.position());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_get_latency<STM: Stream>(
+pub unsafe extern fn capi_stream_get_latency<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
     latency: *mut u32,
-) -> c_int {
+) -> c_int
+{
     let stm = &mut *(s as *mut STM);
 
     *latency = _try!(stm.latency());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_set_volume<STM: Stream>(
+pub unsafe extern fn capi_stream_set_volume<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
     volume: f32,
-) -> c_int {
+) -> c_int
+{
     let stm = &mut *(s as *mut STM);
 
     _try!(stm.set_volume(volume));
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_set_panning<STM: Stream>(
+pub unsafe extern fn capi_stream_set_panning<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
     panning: f32,
-) -> c_int {
+) -> c_int
+{
     let stm = &mut *(s as *mut STM);
 
     _try!(stm.set_panning(panning));
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_get_current_device<STM: Stream>(
+pub unsafe extern fn capi_stream_get_current_device<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
-    device: *mut *const ffi::cubeb_device,
-) -> i32 {
+    device: *mut *mut ffi::cubeb_device,
+) -> i32
+{
     let stm = &mut *(s as *mut STM);
 
-    *device = _try!(stm.current_device());
+    *device = _try!(stm.current_device()).as_ptr();
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_device_destroy<STM: Stream>(
+pub unsafe extern fn capi_stream_device_destroy<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
-    device: *const ffi::cubeb_device,
-) -> c_int {
+    device: *mut ffi::cubeb_device,
+) -> c_int
+{
     let stm = &mut *(s as *mut STM);
+    let device = DeviceRef::from_ptr(device);
     let _ = stm.device_destroy(device);
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_register_device_collection_changed<CTX: Context>(
+pub unsafe extern fn capi_register_device_collection_changed<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     devtype: ffi::cubeb_device_type,
     collection_changed_callback: ffi::cubeb_device_collection_changed_callback,
     user_ptr: *mut c_void,
-) -> i32 {
+) -> i32
+{
     let ctx = &mut *(c as *mut CTX);
     let devtype = DeviceType::from_bits_truncate(devtype);
     _try!(ctx.register_device_collection_changed(
@@ -271,22 +299,6 @@ pub unsafe extern "C" fn capi_register_device_collection_changed<CTX: Context>(
     ffi::CUBEB_OK
 }
 
-trait AsOptRef<'a, T: 'a> {
-    fn as_opt_ref(self) -> Option<&'a T>;
-}
-
-impl<'a, T> AsOptRef<'a, T> for *const T
-where
-    T: 'a,
-{
-    fn as_opt_ref(self) -> Option<&'a T> {
-        if self.is_null() {
-            None
-        } else {
-            Some(unsafe { &*self })
-        }
-    }
-}
 
 fn opt_cstr<T>(_anchor: &T, ptr: *const c_char) -> Option<&CStr> {
     if ptr.is_null() {

kinetiknz/cubeb-repo-merge

src/cubeb-rs/cubeb-backend/src/capi.rs
@@ -3,10 +3,10 @@
 // This program is made available under an ISC-style license.  See the
 // accompanying file LICENSE for details
 
-use {Context, Stream};
-use cubeb_core::{DeviceId, DeviceType, StreamParams, ffi};
-use cubeb_core::binding::Binding;
+use {ContextOps, StreamOps};
+use cubeb_core::{ffi, DeviceCollectionRef, DeviceRef, DeviceType, StreamParams, StreamParamsRef};
 use std::ffi::CStr;
+use std::mem;
 use std::os::raw::{c_char, c_int, c_void};
 
 // Helper macro for unwrapping `Result` values from rust-api calls
@@ -18,6 +18,16 @@ macro_rules! _try(
         Err(e) => return e.raw_code()
     })
 );
+
+macro_rules! as_opt_ref {
+    ($e:expr) => {
+        if $e.is_null() {
+            None
+        } else {
+            Some(StreamParamsRef::from_ptr($e))
+        }
+    }
+}
 
 #[macro_export]
 macro_rules! capi_new(
@@ -47,111 +57,121 @@ macro_rules! capi_new(
             register_device_collection_changed: Some($crate::capi::capi_register_device_collection_changed::<$ctx>)
         }));
 
-pub unsafe extern "C" fn capi_init<CTX: Context>(
+pub unsafe extern fn capi_init<CTX: ContextOps>(
     c: *mut *mut ffi::cubeb,
     context_name: *const c_char,
-) -> c_int {
+) -> c_int
+{
     let anchor = &();
     let context_name = opt_cstr(anchor, context_name);
-    *c = _try!(CTX::init(context_name));
+    let context = _try!(CTX::init(context_name));
+    *c = context.as_ptr();
+    // Leaking pointer across C FFI
+    mem::forget(context);
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_get_backend_id<CTX: Context>(
+pub unsafe extern fn capi_get_backend_id<CTX: ContextOps>(
     c: *mut ffi::cubeb,
 ) -> *const c_char {
     let ctx = &mut *(c as *mut CTX);
     ctx.backend_id().as_ptr()
 }
 
-pub unsafe extern "C" fn capi_get_max_channel_count<CTX: Context>(
+pub unsafe extern fn capi_get_max_channel_count<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     max_channels: *mut u32,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
 
     *max_channels = _try!(ctx.max_channel_count());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_get_min_latency<CTX: Context>(
+pub unsafe extern fn capi_get_min_latency<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     param: ffi::cubeb_stream_params,
     latency_frames: *mut u32,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
-    let param = StreamParams::from_raw(&param);
-    *latency_frames = _try!(ctx.min_latency(&param));
+    let param = StreamParams::from(param);
+    *latency_frames = _try!(ctx.min_latency(param));
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_get_preferred_sample_rate<CTX: Context>(
+pub unsafe extern fn capi_get_preferred_sample_rate<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     rate: *mut u32,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
 
     *rate = _try!(ctx.preferred_sample_rate());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_get_preferred_channel_layout<CTX: Context>(
+pub unsafe extern fn capi_get_preferred_channel_layout<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     layout: *mut ffi::cubeb_channel_layout,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
 
     *layout = _try!(ctx.preferred_channel_layout()) as _;
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_enumerate_devices<CTX: Context>(
+pub unsafe extern fn capi_enumerate_devices<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     devtype: ffi::cubeb_device_type,
     collection: *mut ffi::cubeb_device_collection,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
     let devtype = DeviceType::from_bits_truncate(devtype);
-    *collection = _try!(ctx.enumerate_devices(devtype));
+    let collection = DeviceCollectionRef::from_ptr(collection);
+    _try!(ctx.enumerate_devices(devtype, collection));
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_device_collection_destroy<CTX: Context>(
+pub unsafe extern fn capi_device_collection_destroy<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     collection: *mut ffi::cubeb_device_collection,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
-
-    ctx.device_collection_destroy(collection);
+    let collection = DeviceCollectionRef::from_ptr(collection);
+    let _ = ctx.device_collection_destroy(collection);
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_destroy<CTX>(c: *mut ffi::cubeb) {
+pub unsafe extern fn capi_destroy<CTX>(c: *mut ffi::cubeb) {
     let _: Box<CTX> = Box::from_raw(c as *mut _);
 }
 
-pub unsafe extern "C" fn capi_stream_init<CTX: Context>(
+pub unsafe extern fn capi_stream_init<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     s: *mut *mut ffi::cubeb_stream,
     stream_name: *const c_char,
     input_device: ffi::cubeb_devid,
-    input_stream_params: *const ffi::cubeb_stream_params,
+    input_stream_params: *mut ffi::cubeb_stream_params,
     output_device: ffi::cubeb_devid,
-    output_stream_params: *const ffi::cubeb_stream_params,
+    output_stream_params: *mut ffi::cubeb_stream_params,
     latency_frames: u32,
     data_callback: ffi::cubeb_data_callback,
     state_callback: ffi::cubeb_state_callback,
     user_ptr: *mut c_void,
-) -> c_int {
+) -> c_int
+{
     let ctx = &mut *(c as *mut CTX);
     let anchor = &(); // for lifetime of stream_name as CStr
 
-    let input_device = DeviceId::from_raw(input_device);
-    let input_stream_params = input_stream_params.as_opt_ref();
-    let output_device = DeviceId::from_raw(output_device);
-    let output_stream_params = output_stream_params.as_opt_ref();
+    let input_stream_params = as_opt_ref!(input_stream_params);
+    let output_stream_params = as_opt_ref!(output_stream_params);
 
-    *s = _try!(ctx.stream_init(
+    let stream = _try!(ctx.stream_init(
         opt_cstr(anchor, stream_name),
         input_device,
         input_stream_params,
@@ -162,32 +182,31 @@ pub unsafe extern "C" fn capi_stream_init<CTX: Context>(
         state_callback,
         user_ptr
     ));
+    *s = stream.as_ptr();
+    // Leaking pointer across C FFI
+    mem::forget(stream);
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_destroy<STM>(s: *mut ffi::cubeb_stream) {
+pub unsafe extern fn capi_stream_destroy<STM>(s: *mut ffi::cubeb_stream) {
     let _ = Box::from_raw(s as *mut STM);
 }
 
-pub unsafe extern "C" fn capi_stream_start<STM: Stream>(
-    s: *mut ffi::cubeb_stream,
-) -> c_int {
+pub unsafe extern fn capi_stream_start<STM: StreamOps>(s: *mut ffi::cubeb_stream) -> c_int {
     let stm = &mut *(s as *mut STM);
 
     _try!(stm.start());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_stop<STM: Stream>(
-    s: *mut ffi::cubeb_stream,
-) -> c_int {
+pub unsafe extern fn capi_stream_stop<STM: StreamOps>(s: *mut ffi::cubeb_stream) -> c_int {
     let stm = &mut *(s as *mut STM);
 
     _try!(stm.stop());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_reset_default_device<STM: Stream>(
+pub unsafe extern fn capi_stream_reset_default_device<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
 ) -> c_int {
     let stm = &mut *(s as *mut STM);
@@ -196,71 +215,79 @@ pub unsafe extern "C" fn capi_stream_reset_default_device<STM: Stream>(
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_get_position<STM: Stream>(
+pub unsafe extern fn capi_stream_get_position<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
     position: *mut u64,
-) -> c_int {
+) -> c_int
+{
     let stm = &mut *(s as *mut STM);
 
     *position = _try!(stm.position());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_get_latency<STM: Stream>(
+pub unsafe extern fn capi_stream_get_latency<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
     latency: *mut u32,
-) -> c_int {
+) -> c_int
+{
     let stm = &mut *(s as *mut STM);
 
     *latency = _try!(stm.latency());
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_set_volume<STM: Stream>(
+pub unsafe extern fn capi_stream_set_volume<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
     volume: f32,
-) -> c_int {
+) -> c_int
+{
     let stm = &mut *(s as *mut STM);
 
     _try!(stm.set_volume(volume));
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_set_panning<STM: Stream>(
+pub unsafe extern fn capi_stream_set_panning<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
     panning: f32,
-) -> c_int {
+) -> c_int
+{
     let stm = &mut *(s as *mut STM);
 
     _try!(stm.set_panning(panning));
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_get_current_device<STM: Stream>(
+pub unsafe extern fn capi_stream_get_current_device<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
-    device: *mut *const ffi::cubeb_device,
-) -> i32 {
+    device: *mut *mut ffi::cubeb_device,
+) -> i32
+{
     let stm = &mut *(s as *mut STM);
 
-    *device = _try!(stm.current_device());
+    *device = _try!(stm.current_device()).as_ptr();
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_stream_device_destroy<STM: Stream>(
+pub unsafe extern fn capi_stream_device_destroy<STM: StreamOps>(
     s: *mut ffi::cubeb_stream,
-    device: *const ffi::cubeb_device,
-) -> c_int {
+    device: *mut ffi::cubeb_device,
+) -> c_int
+{
     let stm = &mut *(s as *mut STM);
+    let device = DeviceRef::from_ptr(device);
     let _ = stm.device_destroy(device);
     ffi::CUBEB_OK
 }
 
-pub unsafe extern "C" fn capi_register_device_collection_changed<CTX: Context>(
+pub unsafe extern fn capi_register_device_collection_changed<CTX: ContextOps>(
     c: *mut ffi::cubeb,
     devtype: ffi::cubeb_device_type,
     collection_changed_callback: ffi::cubeb_device_collection_changed_callback,
     user_ptr: *mut c_void,
-) -> i32 {
+) -> i32
+{
     let ctx = &mut *(c as *mut CTX);
     let devtype = DeviceType::from_bits_truncate(devtype);
     _try!(ctx.register_device_collection_changed(
@@ -271,22 +298,6 @@ pub unsafe extern "C" fn capi_register_device_collection_changed<CTX: Context>(
     ffi::CUBEB_OK
 }
 
-trait AsOptRef<'a, T: 'a> {
-    fn as_opt_ref(self) -> Option<&'a T>;
-}
-
-impl<'a, T> AsOptRef<'a, T> for *const T
-where
-    T: 'a,
-{
-    fn as_opt_ref(self) -> Option<&'a T> {
-        if self.is_null() {
-            None
-        } else {
-            Some(unsafe { &*self })
-        }
-    }
-}
 
 fn opt_cstr<T>(_anchor: &T, ptr: *const c_char) -> Option<&CStr> {
     if ptr.is_null() {